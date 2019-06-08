e5d0d015bc75cec9661a8a48ad1f6c8ed25929ed
AidenBow <AidenBow@users.noreply.github.com>
2019-06-07 20:22:36 -0700

    added button interactions

--- js/index.js
+++ js/index.js
@@ -4,17 +4,15 @@ for (let i = 0; i < navLinks.length; i++){
   navLinks[i].addEventListener("click", (event) => {
     console.log(event)
     event.preventDefault();
-    navLinks[i].style.color = 'red';
-    navLinks[i].style.fontSize += 1;
-    console.log(navLinks[i].style.fontSize)
+    navLinks[i].style.color = "red";
   })
 }
 
 let paragraphs = document.querySelectorAll("p")
 
 for (let i = 0; i < paragraphs.length; i++){
-  paragraphs[i].addEventListener('mouseover', (event) => {
-    paragraphs[i].style.color = 'blue';
+  paragraphs[i].addEventListener("mouseover", (event) => {
+    paragraphs[i].style.color = "blue";
   })
 }
 
@@ -33,11 +31,26 @@ let logo = document.querySelector(".logo-heading")
 
 logo.addEventListener("mouseenter", (event) =>{
   event.preventDefault();
-  logo.classList.toggle('logoAlt')
+  logo.classList.toggle("logoAlt")
 })
 
 let headImg = document.querySelector(".intro img")
 
 headImg.addEventListener("drag", (event) => {
   headImg.style.height = "100px"
+})
+
+let buttons = document.querySelectorAll(".btn")
+
+buttons[0].addEventListener("dblclick", (event) => {
+  buttons[0].style.backgroundColor = "black"
+})
+
+buttons[1].addEventListener("copy", () => {
+  buttons[1].style.backgroundColor = "yellow"
+  buttons[1].style.color = "black"
+})
+
+buttons[2].addEventListener("contextmenu", () => {
+  buttons[2].style.backgroundColor = "orange"
 })
