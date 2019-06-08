313d90e6db0166f5f478fd3eba86764f721c8bf1
AidenBow <AidenBow@users.noreply.github.com>
2019-06-07 19:05:38 -0700

    added interaction to navigation, paragraph and body

--- js/index.js
+++ js/index.js
@@ -1,1 +1,29 @@
-// Your code goes here
+let navLinks = document.querySelectorAll(".nav-link")
+console.log(navLinks)
+
+
+for ( let i = 0; i < navLinks.length; i++){
+  navLinks[i].addEventListener("click", (event) => {
+    console.log(event)
+    event.preventDefault();
+    navLinks[i].style.color = 'red';
+    navLinks[i].style.fontSize += 1;
+    console.log(navLinks[i].style.fontSize)
+  })
+}
+
+let paragraphs = document.querySelector("p")
+console.log(paragraphs)
+
+paragraphs.addEventListener('mouseover', (event) => {
+  paragraphs.style.color = 'red';
+})
+
+let webPage = document.querySelector('body')
+console.log(webPage)
+
+webPage.addEventListener("wheel", (event) => {
+  console.log(webPage.style.backgroundColor)
+  webPage.style.backgroundColor = "red"
+})
+
